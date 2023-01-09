66b21fab73c6a4e17676cb49ba125772e145268c
Olena Zamyslova <olenazamyslova@gmail.com>
2023-01-09 02:04:17 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryAllItems = document.querySelector('.gallery');
+
+galleryAllItems.innerHTML = galleryItems
+  .map(({preview, original, description}) => {
+    return `
+    <li class = "gallery__item">
+    <a class = "gallery__link" href= "${original}">
+    <img  
+    class = "gallery__image" 
+    data-source = "${original}" 
+    src = ${preview} 
+    alt = "${description}">
+    </a>
+    </li>
+    `;
+  })
+  .join('');
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionPosition: 'bottom',
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
+
+const contFormEl = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const THE_KEY = 'feedback-form-state';
+const userInfo = {};
+
+const fillContactFormFields = () => {
+  try {
+    const userInfoFromLS = JSON.parse(localStorage.getItem('THE_KEY'));
+
+    if (userInfoFromLS === null) {
+      return;
+    }
+
+      contFormEl.email.value = userInfoFromLS[email];
+      contFormEl.message.value = userInfoFromLS[message];
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fillContactFormFields();
+
+const onContactFormItemChange = event => {
+  const { target } = event;
+
+  const name = target.name;
+  const value = target.value;
+
+  userInfo[name] = value;
+
+  localStorage.setItem('THE_KEY', JSON.stringify(userInfo));
+};
+
+const onContactFormSubmit = event => {
+  event.preventDefault();
+
+  contFormEl.reset();
+    localStorage.removeItem('THE_KEY');
+    console.log(userInfo);
+};
+
+contFormEl.addEventListener('input', throttle(onContactFormItemChange, 500));
+contFormEl.addEventListener('submit', onContactFormSubmit);
 
